51f15f289b8e9fe94ef42f7b64046cdf419d7656
sergeysynergy <sergeysynergy@yandex.ru>
2022-04-15 20:21:22 +0300

    fix: Change tmp dir to /tmp

--- internal/filestore/filestore_test.go
+++ internal/filestore/filestore_test.go
@@ -63,7 +63,7 @@ func TestFileStoreRestoreMetrics(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			f, err := os.CreateTemp("./", "restore-metrics-test*.json")
+			f, err := os.CreateTemp("/tmp", "restore-metrics-test*.json")
 			assert.NoError(t, err)
 			defer os.Remove(f.Name())
 
