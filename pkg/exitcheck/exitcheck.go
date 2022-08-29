e2df230d31bb46b00c19d2c14172558b81592ddc
sergeysynergy <sergeysynergy@yandex.ru>
2022-08-29 17:24:35 +0300

    add another exit check realization

--- pkg/exitcheck/exitcheck.go
+++ pkg/exitcheck/exitcheck.go
@@ -36,12 +36,31 @@ func exitCheck(file *ast.File, pass *analysis.Pass) {
 				// Рекурсивно проходим по элементам узла функция main: ищем прямое объявление os.Exit().
 				ast.Inspect(mainFnc, func(node ast.Node) bool {
 					switch x := node.(type) {
+					/* Вариант 1: ищем просто вызов os.Exit()
 					case *ast.SelectorExpr:
 						if fmt.Sprint(x.X) == "os" && x.Sel.Name == "Exit" {
 							pass.Report(analysis.Diagnostic{
 								Pos:     x.Pos(),
 								Message: "direct function call `os.Exit()` in main package",
 							})
+						}
+					*/
+					// Вариант 2: ищем присваивания функции os.Exit()
+					case *ast.AssignStmt:
+						for i := 0; i < len(x.Lhs); i++ {
+							if _, ok := x.Lhs[i].(*ast.Ident); ok {
+								// вызов функции справа
+								if call, ok := x.Rhs[i].(*ast.CallExpr); ok {
+									if fn, ok := call.Fun.(*ast.SelectorExpr); ok {
+										if fmt.Sprint(fn.X) == "os" && fn.Sel.Name == "Exit" {
+											pass.Report(analysis.Diagnostic{
+												Pos:     x.Pos(),
+												Message: "direct function call `os.Exit()` in main package",
+											})
+										}
+									}
+								}
+							}
 						}
 					}
 					return true
