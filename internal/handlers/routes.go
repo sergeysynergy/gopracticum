24132243d9bb0acd652ad1b77c05790037f77516
sergeysynergy <sergeysynergy@yandex.ru>
2022-09-19 02:31:04 +0300

    try to kill redundant code

--- internal/handlers/routes.go
+++ internal/handlers/routes.go
@@ -4,12 +4,6 @@ package handlers
 func (h *Handler) setRoutes() {
 	h.router.Get("/", h.List)
 
-	// шаблон роутов POST http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
-	h.router.Post("/update/{type}/{name}/{value}", h.Post)
-
-	// шаблон роутов GET http://<АДРЕС_СЕРВЕРА>/value/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>
-	h.router.Get("/value/{type}/{name}", h.Get)
-
 	// обработчики для JSON API
 	h.router.Post("/update/", h.Update)
 	h.router.Post("/updates/", h.Updates)
