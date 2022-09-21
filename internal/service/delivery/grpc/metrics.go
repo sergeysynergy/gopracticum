d5a770195416fbe26485525213023992ee0c6fc1
sergeysynergy <sergeysynergy@yandex.ru>
2022-09-21 03:00:17 +0300

    add gRPC metric list method

--- internal/service/delivery/grpc/metrics.go
+++ internal/service/delivery/grpc/metrics.go
@@ -27,7 +27,20 @@ func (s *MetricsServer) ListMetrics(ctx context.Context, in *pb.ListMetricsReque
 	prm, _ := s.uc.GetMetrics()
 
 	gauges := make([]*pb.Gauge, 0, len(prm.Gauges))
+	for k, v := range prm.Gauges {
+		gauges = append(gauges, &pb.Gauge{
+			Id:    k,
+			Value: float64(v),
+		})
+	}
+
 	counters := make([]*pb.Counter, 0, len(prm.Counters))
+	for k, v := range prm.Counters {
+		counters = append(counters, &pb.Counter{
+			Id:    k,
+			Delta: int64(v),
+		})
+	}
 
 	response := pb.ListMetricsResponse{
 		Gauges:   gauges,
